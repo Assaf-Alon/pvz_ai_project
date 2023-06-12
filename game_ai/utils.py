9857dd8935ce6299d5843613bd1e93aa9b754286
Assaf-Alon <alon.assaf@gmail.com>
2023-06-12 23:17:42 +0300

    Added first 4 levels of the actual game

--- game_ai/utils.py
+++ game_ai/utils.py
@@ -65,10 +65,93 @@ level_data_2_list = [
     (85, 2, "normal"), 
     (85, 3, "normal")
 ]
+
+lvl1_data_list = [
+    (65, 0, "normal"),
+    (80, 0, "normal"),
+    (100, 0, "normal"),
+    (120, 0, "normal"),
+    (121, 0, "normal")
+]
+
+lvl2_data_list = [
+    (40, 0, "normal"),
+    (60, 2, "normal"),
+    (82, 1, "normal"),
+    (95, 0, "normal"),
+    (96, 2, "normal"),
+    (110, 0, "normal"),
+    (111, 2, "normal"),
+    (135, 0, "flag"),
+    (140, 2, "normal"),
+    (141, 2, "normal"),
+    (141, 1, "normal"),
+    (142, 1, "normal")
+]
+
+
+lvl3_data_list = [
+    (20, 0, "normal"),
+    (35, 2, "normal"),
+    (52, 1, "normal"),
+    (76, 0, "normal"),
+    (76, 2, "normal"),
+    (90, 1, "conehead"),
+    (115, 0, "normal"),
+    (117, 2, "normal"),
+    (127, 2, "normal"),
+    (127, 1, "normal"),
+    (128, 0, "normal"),
+    (156, 2, "flag"),
+    (157, 1, "normal"),
+    (157, 0, "conehead"),
+    (158, 1, "normal"),
+    (158, 0, "normal"),
+    (159, 1, "normal")
+]
+
+
+lvl4_data_list = [
+    (20, 2, "normal"),
+    (33, 1, "normal"),
+    (55, 4, "normal"),
+    (87, 3, "conehead"),
+    (106, 1, "normal"),
+    (106, 0, "normal"),
+    (128, 3, "normal"),
+    (129, 2, "normal"),
+    (148, 2, "normal"),
+    (149, 4, "conehead"),
+    (170, 1, "conehead"),
+    (170, 4, "normal"),
+    (197, 3, "normal"),
+    (197, 0, "conehead"),
+    (244, 1, "flag"),
+    (246, 0, "normal"),
+    (246, 0, "conehead"),
+    (246, 2, "normal"),
+    (246, 2, "normal"),
+    (246, 3, "normal"),
+    (246, 3, "normal"),
+    (246, 4, "normal"),
+    (246, 4, "normal")
+]
+
 level_data_2 = construct_level_data_from_list(level_data_2_list)
 
 chosen_plants_1 = [cpp_level.SUNFLOWER, cpp_level.PEASHOOTER, cpp_level.POTATOMINE, cpp_level.SQUASH, cpp_level.SPIKEWEED, cpp_level.WALLNUT]
 chosen_plants_basic = [cpp_level.SUNFLOWER, cpp_level.PEASHOOTER]
+
+# Actual levels
+lvl1_data = construct_level_data_from_list(lvl1_data_list)
+lvl2_data = construct_level_data_from_list(lvl2_data_list)
+lvl3_data = construct_level_data_from_list(lvl3_data_list)
+lvl4_data = construct_level_data_from_list(lvl4_data_list)
+
+chosen_plants_lvl1 = [cpp_level.PEASHOOTER]
+chosen_plants_lvl2 = [cpp_level.PEASHOOTER, cpp_level.SUNFLOWER]
+chosen_plants_lvl3 = [cpp_level.PEASHOOTER, cpp_level.SUNFLOWER, cpp_level.CHERRYBOMB]
+chosen_plants_lvl4 = [cpp_level.PEASHOOTER, cpp_level.SUNFLOWER, cpp_level.CHERRYBOMB, cpp_level.WALLNUT]
 
 
 
@@ -161,8 +244,46 @@ def estimate_simulation_speed():
     end = time()
     print(f"total time: {end-start}, time per rollout: {(end-start)/num_rollout}")
 
+def play_level1():
+    FPS = 10
+    GAMES = 10000
+    level = cpp_level.Level(1, 10, FPS, lvl1_data, chosen_plants_lvl1)
+    print(f"Level 1 Mode 1: {level.rollout(8, GAMES, 1)} / {GAMES}")
+    print(f"Level 1 Mode 2: {level.rollout(8, GAMES, 2)} / {GAMES}")
+    print(f"Level 1 Mode 3: {level.rollout(8, GAMES, 3)} / {GAMES}")
+
+def play_level2():
+    FPS = 10
+    GAMES = 10000
+    level = cpp_level.Level(3, 10, FPS, lvl2_data, chosen_plants_lvl2)
+    print(f"Level 2 Mode 1: {level.rollout(8, GAMES, 1)} / {GAMES}")
+    print(f"Level 2 Mode 2: {level.rollout(8, GAMES, 2)} / {GAMES}")
+    print(f"Level 2 Mode 3: {level.rollout(8, GAMES, 3)} / {GAMES}")
+
+def play_level3():
+    FPS = 10
+    GAMES = 10000
+    level = cpp_level.Level(3, 10, FPS, lvl3_data, chosen_plants_lvl3)
+    print(f"Level 3 Mode 1: {level.rollout(8, GAMES, 1)} / {GAMES}")
+    print(f"Level 3 Mode 2: {level.rollout(8, GAMES, 2)} / {GAMES}")
+    print(f"Level 3 Mode 3: {level.rollout(8, GAMES, 3)} / {GAMES}")
+
+def play_level4():
+    FPS = 10
+    GAMES = 1000000
+    level = cpp_level.Level(5, 10, FPS, lvl4_data, chosen_plants_lvl4)
+    print(f"Level 4 Mode 1: {level.rollout(8, GAMES, 1)} / {GAMES}")
+    print(f"Level 4 Mode 2: {level.rollout(8, GAMES, 2)} / {GAMES}")
+    print(f"Level 4 Mode 3: {level.rollout(8, GAMES, 3)} / {GAMES}")
 
 if __name__ == "__main__":
+    # play_level1()
+    # print()
+    # play_level2()
+    # print()
+    # play_level3()
+    # print()
+    # play_level4()
     # level = cpp_level.Level(5, 10, 10, level_data_1, chosen_plants_1)
     # print(level.rollout(8, 10000, 1))
     # print(level.rollout(8, 10000, 2))
