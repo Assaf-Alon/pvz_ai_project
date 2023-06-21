c2090d08088aa8d83db5d1d98513091b888e7b13
Assaf Alon <alon.assaf@gmail.com>
2023-06-21 19:06:20 +0300

    Added Peashooter tests

--- game_ai/cpp_env/test_full.py
+++ game_ai/cpp_env/test_full.py
@@ -105,6 +105,36 @@ class TestLevel(unittest.TestCase):
                 self.assertFalse(level.is_action_legal(plant, 0, 0), msg=f"Managed to plant {plant} in frame {level.frame} even though it's not legal")
             level.step()
 
+    def test_cooldown1(self):
+        level_data = cpp_level.ZombieQueue()
+        level_data.push_back(cpp_level.ZombieSpawnTemplate(2000, 0, "normal"))
+        level = cpp_level.Level(5, 10, DEFAULT_FPS, level_data, [cpp_level.SUNFLOWER])
+        
+        SUN_COOLDOWN = 7.5 # TODO - get this from plant data
+        
+        while level.frame <= 15 * DEFAULT_FPS:
+            level.step()
+        level.step(cpp_level.SUNFLOWER, 0, 0)
+        while not level.is_action_legal(cpp_level.SUNFLOWER, 1, 0):
+            level.step()
+        
+        self.assertEqual(level.frame, DEFAULT_FPS * (15 + SUN_COOLDOWN) + 1)
+        level.step(cpp_level.SUNFLOWER, 1, 0)
+        self.assertEqual(level.suns, 50)
+    
+    def test_cooldown2(self):
+        level_data = cpp_level.ZombieQueue()
+        level_data.push_back(cpp_level.ZombieSpawnTemplate(2000, 0, "normal"))
+        level = cpp_level.Level(5, 10, DEFAULT_FPS, level_data, [cpp_level.POTATOMINE])
+        
+        MINE_COOLDOWN = 30 # TODO - get this from plant data
+        self.assertTrue(level.is_action_legal(cpp_level.POTATOMINE, 0, 0))
+        level.step(cpp_level.POTATOMINE, 0, 0)
+        while not level.is_action_legal(cpp_level.POTATOMINE, 0, 1):
+            level.step()
+        self.assertEqual(level.frame, DEFAULT_FPS * MINE_COOLDOWN + 1)
+        level.step(cpp_level.POTATOMINE, 0, 1)
+    
 class TestSunflower(unittest.TestCase):
     def test_sunflower1(self):
         # Init level
@@ -159,6 +189,74 @@ class TestSunflower(unittest.TestCase):
             expected_suns = 25 *(gain_from_flower1 + gain_from_flower2 + gain_from_sky)
             self.assertEqual(level.suns, expected_suns, msg=f"Frame: {level.frame}, suns: {level.suns}, expected: {expected_suns}.   {gain_from_sky, gain_from_flower1, gain_from_flower2}")
             level.step()
+
+class TestPeaShooter(unittest.TestCase):
+    def test_peashooter1(self):
+        # Init level
+        level_data = cpp_level.ZombieQueue()
+        level_data.push_back(cpp_level.ZombieSpawnTemplate(200, 0, "normal"))
+        level = cpp_level.Level(5, 10, DEFAULT_FPS, level_data, [cpp_level.PEASHOOTER])
+        
+        while not level.is_action_legal(cpp_level.PEASHOOTER, 0, 0):
+            level.step()
+        level.step(cpp_level.PEASHOOTER, 0, 0)
+        self.assertEqual(level.frame, DEFAULT_FPS * 12 + 3)
+        self.assertEqual(level.suns, 0)
+
+    def test_peashooter2(self):
+        # Init level
+        level_data = cpp_level.ZombieQueue()
+        level_data.push_back(cpp_level.ZombieSpawnTemplate(20, 0, "normal"))
+        level = cpp_level.Level(5, 10, DEFAULT_FPS, level_data, [cpp_level.PEASHOOTER])
+        
+        ZOMBIE_HP = 181
+        ZOMBIE_SPAWN_FRAME = 20 * DEFAULT_FPS
+        PLANT_SHOOT_SPEED = round(1.425 * DEFAULT_FPS)
+        PLANT_DAMAGE = 20
+        
+        while not level.is_action_legal(cpp_level.PEASHOOTER, 0, 0):
+            level.step()
+        level.step(cpp_level.PEASHOOTER, 0, 0)
+        self.assertEqual(level.frame, DEFAULT_FPS * 12 + 3)
+        self.assertEqual(level.suns, 0)
+        
+        while not level.done:
+            level.step()
+        self.assertTrue(level.lawnmowers[0])
+        self.assertTrue(level.win)
+        self.assertLessEqual(ZOMBIE_HP, PLANT_DAMAGE * (1 + ((level.frame - ZOMBIE_SPAWN_FRAME) // PLANT_SHOOT_SPEED)))   # It should be dead now
+        self.assertGreater(ZOMBIE_HP, PLANT_DAMAGE * (1 + ((level.frame - 2 - ZOMBIE_SPAWN_FRAME) // PLANT_SHOOT_SPEED))) # It should be alive in the previous frame
     
+    def test_peashooter3(self):
+        # Init level
+        level_data = cpp_level.ZombieQueue()
+        level_data.push_back(cpp_level.ZombieSpawnTemplate(70, 0, "normal"))
+        level = cpp_level.Level(5, 10, DEFAULT_FPS, level_data, [cpp_level.PEASHOOTER])
+        
+        ZOMBIE_HP = 181
+        ZOMBIE_SPAWN_FRAME = 70 * DEFAULT_FPS
+        PLANT_SHOOT_SPEED = round(1.425 * DEFAULT_FPS)
+        PLANT_DAMAGE = 40
+        
+        while level.frame <= DEFAULT_FPS * 30:
+            level.step()
+        self.assertTrue(level.is_action_legal(cpp_level.PEASHOOTER, 0, 0))
+        level.step(cpp_level.PEASHOOTER, 0, 0)
+        
+        while level.frame <= DEFAULT_FPS * 50:
+            level.step()
+            
+        self.assertTrue(level.is_action_legal(cpp_level.PEASHOOTER, 0, 1))
+        level.step(cpp_level.PEASHOOTER, 0, 1)
+        
+        while not level.done:
+            level.step()
+        self.assertTrue(level.lawnmowers[0])
+        self.assertTrue(level.win)
+        self.assertLessEqual(ZOMBIE_HP, PLANT_DAMAGE * (1 + ((level.frame - ZOMBIE_SPAWN_FRAME) // PLANT_SHOOT_SPEED)))   # It should be dead now
+        self.assertGreater(ZOMBIE_HP, PLANT_DAMAGE * (1 + ((level.frame - 2 - ZOMBIE_SPAWN_FRAME) // PLANT_SHOOT_SPEED))) # It should be alive in the previous frame
+
+
+
 if __name__ == '__main__':
     unittest.main()
